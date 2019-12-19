f0a185aab19063880b14ff2b16854ae48fc2b2fa
IrvgottiTheDon <irv.irra@gmail.com>
2019-12-18 21:29:42 -0700

    Updates

---
+++ precache-manifest.5d082839655f44c470420fdaf3575940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c219360933e841c3c95c8b9ad2155d0d",
+    "url": "/dragonballz-clickygame/index.html"
+  },
+  {
+    "revision": "2b020f410b11f2b56b0d",
+    "url": "/dragonballz-clickygame/static/css/main.07056ff4.chunk.css"
+  },
+  {
+    "revision": "8d5f9d6a7f7da24b3756",
+    "url": "/dragonballz-clickygame/static/js/2.6f52f5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dragonballz-clickygame/static/js/2.6f52f5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b020f410b11f2b56b0d",
+    "url": "/dragonballz-clickygame/static/js/main.42d49228.chunk.js"
+  },
+  {
+    "revision": "ae5a5733c49e02d24c48",
+    "url": "/dragonballz-clickygame/static/js/runtime-main.9b554b51.js"
+  }
+]);
